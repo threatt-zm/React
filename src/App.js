1558d53ff3a2a7dd09ce257d9abb869a0ec72888
Zabian Threatt <zab3tt@gmail.com>
2021-09-02 14:52:03 -0400

    React Router

--- src/App.js
+++ src/App.js
@@ -4,14 +4,17 @@ import { Navbar, NavbarBrand } from 'reactstrap';
 import Directory from './components/DirectoryComponent';
 import { CAMPSITES } from './shared/campsites';
 import Main from './components/MainComponent';
+import { BrowserRouter } from 'react-router-dom';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <Main />
-      </div>
+      <BrowserRouter>
+        <div className="App">
+          <Main />
+        </div>
+      </BrowserRouter>
     );
   }
 }
