3fbf6c6b0d8ecd429771eb2d0535d27c19a462e4
threatt-zm <82014435+threatt-zm@users.noreply.github.com>
2021-09-17 22:56:34 -0400

    React Redux Forms Revisited

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -3,8 +3,10 @@ import { Campsites } from './campsites';
 import { Comments } from './comments';
 import { Partners } from './partners';
 import { Promotions } from './promotions';
+import { createForms } from 'react-redux-form';
 import thunk from 'redux-thunk';
 import logger from 'redux-logger';
+import { InitialFeedback } from './forms';
 
 export const ConfigureStore = () => {
     const store = createStore(
@@ -12,7 +14,10 @@ export const ConfigureStore = () => {
             campsites: Campsites,
             comments: Comments,
             partners: Partners,
-            promotions: Promotions
+            promotions: Promotions,
+            ...createForms({
+                feedbackForm: InitialFeedback
+            })
         }),
         applyMiddleware(thunk, logger)
     );
