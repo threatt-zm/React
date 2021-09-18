ad31952a1e0672cefd522bfc31b475952719f0e5
threatt-zm <82014435+threatt-zm@users.noreply.github.com>
2021-09-17 22:05:12 -0400

    Redux Actions

--- src/components/CampsiteInfoComponent.js
+++ src/components/CampsiteInfoComponent.js
@@ -26,9 +26,8 @@ class CommentForm extends Component {
     }
 
     handleSubmit(values) {
-        console.log("Current state is: " + JSON.stringify(values));
-        alert("Current state is: " + JSON.stringify(values));
         this.toggleModal();
+        this.props.addComment(this.props.campsiteId, values.rating, values.author, values.text);
     }
 
     render() {
@@ -98,11 +97,11 @@ function RenderCampsite({campsite}) {
     );
 }
 
-function RenderComments({comments, campsite}) {
+function RenderComments({comments, addComment, campsiteId}) {
     if(comments) {
         return(
             <div class="col-md-5 m-1" align="left">
-                <h4>Comments - {campsite.name}</h4>
+                <h4>Comments</h4>
                 {
                     comments.map(comment => {
                         return(
@@ -114,7 +113,7 @@ function RenderComments({comments, campsite}) {
                         )
                     })
                 }
-                <CommentForm />
+                <CommentForm campsiteId={campsiteId} addComment={addComment} />
             </div>
         );
     }
@@ -137,7 +136,11 @@ function CampsiteInfo(props) {
                 </div>
                 <div className="row">
                     <RenderCampsite campsite={props.campsite} />
-                    <RenderComments comments={props.comments} campsite={props.campsite} />
+                    <RenderComments 
+                        comments={props.comments}
+                        addComment={props.addComment}
+                        campsiteId={props.campsite.id}
+                    />
                 </div>
             </div>
         );
